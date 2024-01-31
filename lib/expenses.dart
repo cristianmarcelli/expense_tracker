d00c4eea6ba351514ad0bb0f1f9fc8da50e6255b
cristian.marcelli <cristianma98@gmail.com>
2024-01-31 16:41:58 +0100

    Added expenses.dart

---
+++ lib/expenses.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class Expanses extends StatefulWidget {
+
+  const Expanses({super.key});
+
+  @override
+  State<Expanses> createState() {
+    return _ExpansesState();
+  }
+}
+
+class _ExpansesState extends State<Expanses> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
