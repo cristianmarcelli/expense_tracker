9ded4b46af2dbf7962816215be1a9f33ae1654d3
cristianmarcelli <cristianma98@gmail.com>
2024-02-01 16:54:43 +0100

    Aggiunte cards

---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text(expense.title),
+    );
+  }
+}
