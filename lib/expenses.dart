490f35fa6573aefd8828e9506b5e617eba868cc3
cristianmarcelli <cristianma98@gmail.com>
2024-02-01 15:49:06 +0100

    Aggiunto build in expenses.dart + aggiunta lista mockata spese

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'models/expense.dart';
 
 class Expanses extends StatefulWidget {
-
   const Expanses({super.key});
 
   @override
@@ -11,9 +12,38 @@ class Expanses extends StatefulWidget {
 }
 
 class _ExpansesState extends State<Expanses> {
+  final List<Expense> _registeredExpense = [
+    Expense(
+        title: 'Hamburger',
+        amount: 13.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Pizza',
+        amount: 11.00,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Train to Bologna',
+        amount: 34.50,
+        date: DateTime.now(),
+        category: Category.travel),
+    Expense(
+        title: 'New laptop',
+        amount: 399.00,
+        date: DateTime.now(),
+        category: Category.work),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text('The chart...'),
+          Text('Expenses list...'),
+        ],
+      ),
+    );
   }
 }
