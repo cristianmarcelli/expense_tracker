52dc797a45f1557b699948eceb402c0c967872de
cristianmarcelli <cristianma98@gmail.com>
2024-02-01 15:40:49 +0100

    Conclusa classe expense.dart con enum e uuid

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,14 +1,28 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+}
+
 class Expense {
   Expense({
-    required this.id,
     required this.title,
     required this.amount,
     required this.date,
-  });
+    required this.category,
+  }) : id = uuid.v4();
+
+  // : id = uuid.v4(); -- In questo modo si genera un ID univoco e lo si assegna come valore iniziale alla proprietà ID
+  // ogni volta che la classe Expense viene inizializzata
 
   final String id;
   final String title;
   final double amount;
-
   final DateTime date;
+  final Category category;
 }
