53c410308b5c6320a052a5ad85b5772a6f340b5a
cristianmarcelli <cristianma98@gmail.com>
2024-02-05 15:35:31 +0100

    Cambiato tema generale bottoni, testi, cards

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -11,7 +11,7 @@ class Expenses extends StatefulWidget {
 }
 
 var kColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 230, 207, 0));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 196, 39));
 
 class _ExpensesState extends State<Expenses> {
   @override
@@ -23,6 +23,25 @@ class _ExpensesState extends State<Expenses> {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
       ),
       home: const HomePage(),
     );
