ddaee4121c0e280776b53abf2ab19df609008ea4
cristianmarcelli <cristianma98@gmail.com>
2024-02-07 11:31:48 +0100

    Inserito codice commentato per fissare orientamento app verticalmente

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,10 @@ import 'package:flutter/material.dart';
 import 'widgets/expenses.dart';
 
 void main() {
+  // Codice per obbligare l'app a stare verticalmente
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {});
   runApp(const Expenses());
 }
